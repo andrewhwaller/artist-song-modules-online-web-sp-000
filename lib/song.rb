dd7603e669a34489293ca2778e2846799708b9ce
Andrew Waller <andrewhwaller@gmail.com>
2019-03-23 02:19:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,7 +5,7 @@ class Song
   attr_reader :artist
 
   extend Memorable
-  
+
   @@songs = []
 
   def initialize
@@ -19,14 +19,6 @@ class Song
   def self.all
     @@songs
   end
-
-  # def self.reset_all
-  #   self.all.clear
-  # end
-  #
-  # def self.count
-  #   self.all.count
-  # end
 
   def artist=(artist)
     @artist = artist
